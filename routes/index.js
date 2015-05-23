4e8d30ccd53430766c506aa47632e02279299fcc
mingkaic <mingkaic@sfu.ca>
2015-05-22 22:32:53 -0700

    introduced multipart and file uploads into static files

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,48 @@
 var express = require('express');
+var fs = require('fs');
+var multipart = require('connect-multiparty');
+
+var multipartMiddleware = multipart();
 var router = express.Router();
+
+var imgFolderPath = __dirname + "/../public/images/";
+
+var makeid = function() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < 10; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'The Flash Gallery' });
+	fs.readdir(imgFolderPath, function(err, files) {
+		var imgUrl = [];
+		var length = files.length;
+		for (var i = 0; i < length; i++) {
+			imgUrl.push("/images/"+files[i]);
+		}
+		var vm = {
+			title: 'The Flash Gallery',
+			imageUrl: imgUrl
+		};
+
+		res.render('index', vm);
+	});
+});
+
+router.post('/uploads', multipartMiddleware, function(req, res, next) {
+	fs.readFile(req.files.displayImage.path, function (err, data) {
+		var imgPath = imgFolderPath+makeid()+".jpg";
+		fs.writeFile(imgPath, data, function (err) {
+			if (err) console.log(err);
+			delete req.files;
+			res.redirect("back");
+		});
+	});
 });
 
 module.exports = router;
